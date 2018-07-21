492a31e62dda5532b08181f507b8da1e41363728
Sileo1024 <natalia.niziolek@gmail.com>
2018-07-21 12:45:55 +0200

    Created an enum for aim (of mission)

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -2,4 +2,6 @@ class Mission < ApplicationRecord
 	has_one :captain, foreign_key: "user_id", class_name: "User"
 	has_many :users
 	has_many :items
+
+	enum aim: %i(colonize maintain utilize other)
 end
