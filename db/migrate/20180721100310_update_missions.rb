d0b7026fae7aefb153598d93aee289f67a77b34a
Sileo1024 <natalia.niziolek@gmail.com>
2018-07-21 12:06:33 +0200

    Changed Missions table - added name, description, capitan_id, aim fields

---
+++ db/migrate/20180721100310_update_missions.rb
@@ -0,0 +1,10 @@
+class UpdateMissions < ActiveRecord::Migration[5.2]
+  def change
+  	change_table :missions do |t| 
+  		t.string :name
+  		t.text :description
+  		t.integer :captain_id
+  		t.integer :aim
+  	end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_21_090434) do
+ActiveRecord::Schema.define(version: 2018_07_21_100310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 2018_07_21_090434) do
   create_table "missions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "description"
+    t.integer "captain_id"
+    t.integer "aim"
   end
 
   create_table "roles", force: :cascade do |t|
