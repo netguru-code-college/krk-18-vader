fffece4acd50a8ec7afb860c0060e12cd7601e2e
Tomasz Jaśkiewicz <tomasz.jaskiewicz@netguru.pl>
2018-07-22 11:10:44 +0200

    Resolve rubocop issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
@@ -48,7 +50,7 @@ class UsersController < ApplicationController
   end
 
   private
-  
+
   def set_user
     @user = User.find(params[:id])
   end
