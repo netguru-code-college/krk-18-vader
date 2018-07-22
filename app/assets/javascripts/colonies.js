ec767829db4325283e185029a6304304b5717113
Robert Głód <robertglod104@gmail.com>
2018-07-22 09:55:08 +0200

    Added google maps to colonies#index

---
+++ app/assets/javascripts/colonies.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+  initMap() 
+}
+
+const uluru = {lat: -25.344, lng: 131.036};
+
+initMap = () => {
+  const map = new google.maps.Map(document.querySelector("#g-map-index"), {
+    zoom: 4,
+    center: uluru
+  })
+}
