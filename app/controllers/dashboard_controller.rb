f417b66da551ca7febc2fe7149a3a2405d5a70bd
Thu Le <lethikhanhthu@gmail.com>
2018-07-22 14:18:24 +0200

    Improve the missions listing

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,9 +4,4 @@ class DashboardController < ApplicationController
   def index
     @missions = current_user.missions
   end
-
-  def new
-    @mission = current_user.missions.build.decorate
-  end
-
 end
