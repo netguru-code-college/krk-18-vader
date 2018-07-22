c86734021799231ad9bd2d3a25a38183b1d3c248
Thu Le <lethikhanhthu@gmail.com>
2018-07-22 14:13:58 +0200

    Add missions listings

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController
-  def index; end
+  def index
+    @missions = current_user.missions
+  end
+
+  def new
+    @mission = current_user.missions.build.decorate
+  end
+
 end
