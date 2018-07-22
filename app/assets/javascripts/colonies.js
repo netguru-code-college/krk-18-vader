bc448c12f120dad872c69c05991a44c71008577d
Robert Głód <robertglod104@gmail.com>
2018-07-22 11:28:58 +0200

    Add markers feature

--- app/assets/javascripts/colonies.js
+++ app/assets/javascripts/colonies.js
@@ -1,8 +1,41 @@
-const uluru = {lat: -25.344, lng: 131.036};
+const uluru = {lat: 28.291565, lng: -15.5};
+let markers = []
+let map
+const fetchMarkers = async () => {
+  await $.get('/colonies.json')
+  .done(resp => {
+    resp.map( mark => {
+      markers.push(mark)
+    })
+  })
+  .fail(err => {
+    console.log(err)
+  })
+}
 
-initMap = () => {
-  const map = new google.maps.Map(document.querySelector("#g-map-index"), {
-    zoom: 4,
+const createMarkers = () => {
+  markers.forEach(marker => {
+    let markerName = marker.name
+    let markerLat = marker.lat
+    let markerLng = marker.lng
+    let gMarker = new google.maps.Marker({
+      position: {lat: markerLat, lng: markerLng},
+      map: map
+    });
+   let infowindow = new google.maps.InfoWindow({
+      content: markerName
+    });
+    gMarker.addListener('click', function() {
+      infowindow.open(map, gMarker);
+    });
+  })
+}
+
+initMap = async () => {
+  await fetchMarkers()
+  map = new google.maps.Map(document.querySelector("#g-map-index"), {
+    zoom: 3,
     center: uluru
   });
+  createMarkers()
 };
